c30e52fbe6f8a7cf50d5b0135b23d2583c934c0e
archiKumari <archikumari97@gmail.com>
2025-05-18 14:34:15 +0530

    First commit

---
+++ src/components/IndiaMap.tsx
@@ -0,0 +1,32 @@
+import IndiaSVG from "../assets/india.svg?react";
+
+type Props = {
+  onStateClick: (stateId: string) => void;
+};
+
+export default function IndiaMap({ onStateClick }: Props) {
+  const handleClick = (event: React.MouseEvent<SVGSVGElement, MouseEvent>) => {
+    const target = event.target as SVGElement;
+    const clickedPath = target.closest("path");
+
+    if (clickedPath) {
+      const stateId = clickedPath.id;
+      const stateTitle = clickedPath.getAttribute("title") || stateId;
+
+      if (stateId.startsWith("IN-")) {
+        onStateClick(stateTitle);
+        console.log("ID:", stateId);
+        console.log("Title:", stateTitle);
+      }
+    }
+  };
+
+  return (
+    <div className="w-full h-full">
+      <IndiaSVG
+        className="w-full h-full object-contain"
+        onClick={handleClick}
+      />
+    </div>
+  );
+}
